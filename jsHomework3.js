e972fd596ad6bef91c98f914fc5b361ca91acf6a
Kateryna Veresotska <145865349+KaterynaVeresotska@users.noreply.github.com>
2023-10-15 15:20:03 +0200

    addHomework3

---
+++ jsHomework3.js
@@ -0,0 +1,46 @@
+//ДЗ 1//
+
+// 1 спосіб//
+/*let initString= ""
+function drawTriangle(height,symbol) {
+
+    let i =0;
+    while (i<height) {
+        console.log(initString+=symbol);
+        i++}
+}
+drawTriangle(5,`&`)
+*/
+
+// 2 спосіб//
+/*let initString= ""
+function drawTriangle2(height,symbol) {
+for(let i = height; i>0; i--) {
+    initString+=symbol
+    console.log(initString)
+  }
+}
+drawTriangle2(5,"#")
+*/
+
+//ДЗ 2//
+
+/*let sum = 0
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 !== 0) {
+        sum += i
+    }
+}
+console.log(sum)
+*/
+
+//ДЗ 3//
+/*function pow(x, y) {
+    let result = x;
+    for (let i = 1; i < y; i++) {
+        result *= x
+    }
+    return result
+}
+console.log (pow(2,3))
+*/
